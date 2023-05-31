216f6b2a2303e195a3e31f964b939891eb8eec39
Dani BL <danibachsl@protonmail.com>
2023-05-31 17:14:46 +0200

    CrossChainDAOToken.sol done

--- contracts/OFTVotes.sol
+++ contracts/OFTVotes.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
@@ -13,28 +13,46 @@ abstract contract OFTVotes is OFTCore, ERC20Votes, IOFT {
         string memory _symbol,
         address _lzEndpoint
     ) ERC20(_name, _symbol) OFTCore(_lzEndpoint) {}
-    
-        function supportsInterface(bytes4 interfaceId) public view virtual override(OFTCore, IERC165) returns (bool) {
-            return interfaceId == type(IOFT).interfaceId || interfaceId == type(IERC20).interfaceId || super.supportsInterface(interfaceId);
-        }
 
-        function token() public view virtual override returns (address) {
-            return address(this);
-        }
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(OFTCore, IERC165)
+        returns (bool)
+    {
+        return
+            interfaceId == type(IOFT).interfaceId ||
+            interfaceId == type(IERC20).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
 
-        function circulatingSupply() public view virtual override returns (uint) {
-            return totalSupply();
-        }
+    function token() public view virtual override returns (address) {
+        return address(this);
+    }
 
-        function _debitFrom(address _from, uint16, bytes memory, uint _amount) internal virtual override returns(uint) {
-            address spender = _msgSender();
-            if (_from != spender) _spendAllowance(_from, spender, _amount);
-            _burn(_from, _amount);
-            return _amount;
-        }
+    function circulatingSupply() public view virtual override returns (uint) {
+        return totalSupply();
+    }
 
-        function _creditTo(uint16, address _toAddress, uint _amount) internal virtual override returns(uint) {
-            _mint(_toAddress, _amount);
-            return _amount;
-        }
+    function _debitFrom(
+        address _from,
+        uint16,
+        bytes memory,
+        uint _amount
+    ) internal virtual override returns (uint) {
+        address spender = _msgSender();
+        if (_from != spender) _spendAllowance(_from, spender, _amount);
+        _burn(_from, _amount);
+        return _amount;
+    }
+
+    function _creditTo(
+        uint16,
+        address _toAddress,
+        uint _amount
+    ) internal virtual override returns (uint) {
+        _mint(_toAddress, _amount);
+        return _amount;
+    }
 }
